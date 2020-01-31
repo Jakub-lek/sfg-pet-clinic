08dabc5638df4ee046a26527b2c0d8e3f1a69ca9
jakub jilek <jilek@oksystem.cz>
2020-01-31 08:17:31 +0100

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package jaji.springframework.sfgpetclinic.services;
+
+import jaji.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package jaji.springframework.sfgpetclinic.services;
+
+import jaji.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
