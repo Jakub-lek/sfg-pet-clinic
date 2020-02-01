0aa64083dc3391464b083ff202db1ae1e4875b02
jakub jilek <jilek@oksystem.cz>
2020-02-01 22:40:08 +0100

    Refractor to common Service interface. Closes #11

--- pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/PetService.java
@@ -2,13 +2,5 @@ package jaji.springframework.sfgpetclinic.services;
 
 import jaji.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>  {
 }
