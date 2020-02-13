d8fe5bc1b927a077de40f91e5e4d5590c9bda99d
jakub jilek <jilek@oksystem.cz>
2020-02-13 07:42:26 +0100

    Adding bootstrap for data. Closes #14

--- pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/map/PetMapService.java
@@ -1,11 +1,11 @@
 package jaji.springframework.sfgpetclinic.services.map;
 
 import jaji.springframework.sfgpetclinic.model.Pet;
-import jaji.springframework.sfgpetclinic.services.CrudService;
+import jaji.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
