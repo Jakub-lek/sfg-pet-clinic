75da24c2e34772525c2ba09c121c52a5153cb930
jakub jilek <jilek@oksystem.cz>
2020-02-23 19:45:09 +0100

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package jaji.springframework.sfgpetclinic.services;
+
+import jaji.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author Jakub Jílek on 23.02.2020
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,39 @@
+package jaji.springframework.sfgpetclinic.services.map;
+
+import jaji.springframework.sfgpetclinic.model.PetType;
+import jaji.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author Jakub Jílek on 23.02.2020
+ */
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deletById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
