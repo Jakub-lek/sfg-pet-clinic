68a4ae1ff9e41e012d643d980749b360755ea3ef
jakub jilek <jilek@oksystem.cz>
2020-03-10 13:32:55 +0100

    Implemented JPA Visit Service, added loader data. Closes #47

--- pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -2,12 +2,14 @@ package jaji.springframework.sfgpetclinic.services.map;
 
 import jaji.springframework.sfgpetclinic.model.Visit;
 import jaji.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 /**
  * @author Jakub Jílek on 10.03.2020
  */
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
