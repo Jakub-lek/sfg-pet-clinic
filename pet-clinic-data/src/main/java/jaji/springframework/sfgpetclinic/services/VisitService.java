b16d9964d4c63aa27c552d23d93e8ab5d5bb7f37
jakub jilek <jilek@oksystem.cz>
2020-03-10 13:22:14 +0100

    Created Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,9 @@
+package jaji.springframework.sfgpetclinic.services;
+
+import jaji.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * @author Jakub Jílek on 10.03.2020
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/jaji/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,40 @@
+package jaji.springframework.sfgpetclinic.services.map;
+
+import jaji.springframework.sfgpetclinic.model.Visit;
+import jaji.springframework.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+/**
+ * @author Jakub Jílek on 10.03.2020
+ */
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deletById(aLong);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null || object.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
